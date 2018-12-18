a1224fbd9ab4fb429887e6ca6e4f6de55be9517f
amodinho <u14284783@tuks.co.za>
2018-12-18 17:35:37 +0200

    trying to get the serverless graphql API

--- graphql.js
+++ graphql.js
@@ -1,6 +1,43 @@
  const {ApolloServer, gql} = require('apollo-server-lambda');
 import {success, failure} from "./libs/response-lib";
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+} = require('graphql');
 
+
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType', // an arbitrary name
+    fields: {
+      // the query has a field called 'greeting'
+      hello: {
+        // we need to know the user's name to greet them
+        
+        // the greeting message is a string
+        type: GraphQLString,
+        // resolve to a greeting message
+        resolve(){
+         return 'Hello World'
+        },
+      },
+    },
+  }),
+})
+
+module.exports.query = (event, context,callback) => 
+ graphql(schema, event.queryStringParameters.query)
+ .then(
+   result => callback(null, {statusCode:200,body: JSON.stringify(result)}),
+   err => callback(err)
+ );
+
+
+/*
  const typeDefs = gql`
     type Query {
       hello: String
@@ -42,3 +79,4 @@ import {success, failure} from "./libs/response-lib";
    return handler(event, context,callback);
  };
 
+*/
