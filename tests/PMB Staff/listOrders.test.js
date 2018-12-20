9cc07e4d32b4c6ae12330c7c25b63459dd6315cb
amodinho <u14284783@tuks.co.za>
2018-12-20 15:18:38 +0200

    updated test cases

---
+++ tests/PMB Staff/listOrders.test.js
@@ -0,0 +1,20 @@
+
+describe("PMB Staff should be able list all orders from table", () => {
+
+
+    test("returns a 200 respone", async () => {
+    
+    });
+    
+    test("returns a 500 respone", async () => {
+    
+    });
+    
+    test("It is to extract data from dynamodb ", async () => {
+    
+    });
+    
+    test("It cannot extract from dynamodb", async () => {
+        
+    });
+    })
---
+++ tests/PMB Staff/updateAnOrder.test.js
@@ -0,0 +1,36 @@
+
+describe("PMB Staff can update and add order information", () => {
+
+
+    test("returns a 200 respone", async () => {
+    
+    });
+    
+    test("returns a 500 respone", async () => {
+    
+    });
+    
+    test("It is to extract data from dynamodb ", async () => {
+    
+    });
+    
+    test("It cannot extract from dynamodb", async () => {
+        
+    });
+
+    test("can update order status", async () => {
+        
+    });
+
+    test("sends email once updated status", async () => {
+        
+    });
+
+    test("Calculates lead time", async () => {
+        
+    });
+
+    test("Able to add other order information", async () => {
+        
+    });
+})
