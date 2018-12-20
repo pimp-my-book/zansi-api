9261696e0e83690fa352a4b2e54aef5f7a6de949
amodinho <u14284783@tuks.co.za>
2018-12-20 15:02:11 +0200

    spec for exporting to excel test

---
+++ tests/PMB Staff/exportToExcel.test.js
@@ -0,0 +1,20 @@
+
+describe("Export all orders to an Excel Spreadsheet", () => {
+
+
+    test("returns a 200 respone", async () => {
+    
+    });
+    
+    test("returns a 500 respone", async () => {
+    
+    });
+    
+    test("It is to extract data from dynamodb and make spreadsheet", async () => {
+    
+    });
+    
+    test("It cannot extract from dynamodb", async () => {
+        
+    });
+    })
