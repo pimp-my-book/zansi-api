73f38a4c08b550f9a9e0b4b2b1b782616163e0c6
amodinho <u14284783@tuks.co.za>
2018-12-20 14:05:29 +0200

    Added aws-sdk and a helper for dynamo db

---
+++ tests/createOrder.test.js
@@ -0,0 +1,21 @@
+
+
+describe("Create Order", () => {
+
+
+test("returns a 200 respone", async () => {
+
+});
+
+test("returns a 500 respone", async () => {
+
+});
+
+test("It is able to submit to dynamodb", async () => {
+
+});
+
+test("It does not submit to dynamodb", async () => {
+    
+});
+})
