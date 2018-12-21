3bb2d4c2f4ba3da8ee33760ff846da4d0368d955
amodinho <u14284783@tuks.co.za>
2018-12-21 11:39:24 +0200

    trying to get it to work

--- graphql.js
+++ graphql.js
@@ -27,56 +27,13 @@ const schema = new GraphQLSchema({
       },
     },
   }),
-})
+});
 
-module.exports.query = (event, context,callback,query) => 
- graphql(schema, event.queryStringParameters.query, null, {})
+exports.query = async (event, context,callback) => 
+ graphql(schema, event.queryStringParameters.query)
  .then(
    result => callback(null, {statusCode:200,body: JSON.stringify(result)}),
    err => callback(err)
  );
 
-
-/*
- const typeDefs = gql`
-    type Query {
-      hello: String
-    }
- `;
-
- const resolvers = {
-  Query: {
-    hello: () => 'Zansi is Alive!!'
-  },
- };
-
- const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   context: ({event,context}) => ({
-      headers: event.headers,
-      functionName: context.functionName,
-      event,
-      context,
-   }),
-   });
-
- exports.graphqlHandler = (event,context,callback) =>{
-  const handler = server.createHandler({
-    cors: {
-      origin: '*',
-      methods: [
-        'POST'
-      ], 
-      allowedHeaders: [
-        'Content-Type',
-        'Origin',
-        'Accept'
-      ]
-    },
-   }) ;
-
-   return handler(event, context,callback);
- };
-
-*/
+              
