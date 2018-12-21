3a29b266da2a7b82335659c7d03e680f50926376
amodinho <u14284783@tuks.co.za>
2018-12-21 12:35:48 +0200

    updating code

--- graphql.js
+++ graphql.js
@@ -1,54 +1,55 @@
-const {ApolloServer, gql} = require('apollo-server-lambda');
-import {success, failure} from "./libs/response-lib";
+const {ApolloServer, gql} = require("apollo-server-lambda");
 
- const typeDefs = gql`
+const typeDefs = gql`
     type Query {
       hello: String
     }
  `;
 
- const resolvers = {
-  Query: {
-    hello: () => 'Zansi is now live!🎈 Zansi is a Pimp My Book ordering service for university textbooks 📚'
-  },
- };
+const resolvers = {
+	Query: {
+		hello: () => "Zansi is now live!🎈 Zansi is a Pimp My Book ordering service for university textbooks 📚"
+	},
+};
 
- const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   formatError: error => {
-     console.log(error);
-     return error;
-   },
-   formatResponse: response => {
-     console.log(response);
-     return response;
-   },
-   context: ({event,context}) => ({
-      headers: event.headers,
-      functionName: context.functionName,
-      event,
-      context,
-   }),
-   tracing: true
-   });
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	formatError: error => {
+		console.log(error);
+		return error;
+	},
+	formatResponse: response => {
+		console.log(response);
+		return response;
+	},
+	context: ({event,context}) => ({
+		headers: event.headers,
+		functionName: context.functionName,
+		event,
+		context,
+	}),
+	tracing: true,
+	playground: true
+});
 
- exports.graphqlHandler = (event,context,callback) =>{
-  const handler = server.createHandler({
-    cors: {
-      origin: '*',
-      methods: [
-        'POST',
-        'GET'
-      ], 
-      allowedHeaders: [
-        'Content-Type',
-        'Origin',
-        'Accept'
-      ]
-    },
-   }) ;
+exports.graphqlHandler = (event,context,callback) =>{
+	const handler = server.createHandler({
+		cors: {
+			origin: "*",
+			credentials:true,
+			methods: [
+				"POST",
+				"GET"
+			], 
+			allowedHeaders: [
+				"Content-Type",
+				"Origin",
+				"Accept"
+			]
+		},
+	}) ;
 
-   return handler(event, context,callback);
- };
+	return handler(event, context,callback);
+};
 
