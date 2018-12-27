b53d7e4c6438179455b334278619bc1fa7e0a991
amodinho <u14284783@tuks.co.za>
2018-12-27 15:46:55 +0200

    done with the schema

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,93 @@
 const schema = `
+
+   scalar Date
+
+   type Student {
+     id: Int!
+     studentNumber: Int!
+     name: String!
+     email: String!
+     univeristy: String!
+     degree: String!
+     currentYear: Int!
+     bursary: String!
+     cellNumber: Int!
+     address: String!
+   }
+
+   enum AllowedVendors {
+    PMBPAROW
+    PMPSTELLIES
+    PMBUCT
+    PMBHO
+    PMBPTA
+    PMBUFS
+    ADAMS
+    REHAB
+    PROTEA
+    VANSCHAIK
+    TAKEALOT
+    VITALSOURCE
+    LOOT
+    OTHER
+   }
+
+   enum Conditions {
+     NEW
+     USED
+   }
+
+   enum DeliveryType {
+    COURIER
+    PMBPAROW
+    PMPSTELLIES
+    PMBUCT
+    PMBHO
+    PMBPTA
+    PMBUFS
+    EMAIL
+    PMBDELIVERY 
+   }
+
+
+
+   type Order {
+     id: ID! 
+     ISBN: Int!
+     title: String!
+     edition: String!
+     author:  String!
+     dateOrdered: Date
+     status: String
+     statusDate: Date
+     ETA: String
+     Vendor: [AllowedVendors]
+     condition: [Conditions]
+     deliveryMethod: [DeliveryType]
+     deliveryDate: Date
+     leadTime: String
+   }
+
+   type OrderList {
+     orders: [Order!]!
+   }
+
+
     type Query {
       hello: String
+      studentOrder(id:ID!): Order
+      viewOrder(id:ID!): Order
+      orderList: OrderList!
     }
+
+    type Mutation {
+      placeOrder(ISBN: Int!,title: String!,edition: String!,author:String!): Order
+      cancelOrder(id:ID!):Order
+      updateOrderStatus(id:ID!,status:String,email:String):Order
+      updateOrderInfo(id:ID!,ETA: String,Vendor: [AllowedVendors],condition: [Conditions],deliveryMethod: [DeliveryType], deliveryDate: Date): Order
+      exportToExcel(id:ID!):Order
+    }
+   
  `;
 
  export  {schema};
