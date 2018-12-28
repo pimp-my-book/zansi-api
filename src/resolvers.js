4cd057ce3901c03cc6d07c9d0cce18187636d4d7
amodinho <u14284783@tuks.co.za>
2018-12-28 15:35:53 +0200

    trying to get the auth context to work and read multiple tables.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,37 @@
+import * as dynamoDBLib from "../libs/dynamodb-lib";
+import uuid from "uuid";
+
+const TableName = process.env.tableName2;
+
+const data = {
+	  studentDetails(_,args,context){
+		const params = {
+			TableName,
+			Item: {
+				id: args.id,
+				studentNumber: args.studentNumber,
+				name: args.name,
+				email: args.name,
+				university: args.university,
+				degree: args.degree,
+				currentYear: args.currentYear,
+				bursary: args.bursary,
+				cellNumber: args.cellNumber,
+				address: args.address
+
+			}
+
+		}
+
+		return dynamoDBLib.call("put", params);
+	}
+}
+
 export const resolvers = {
 	Query: {
 		hello: () => "Zansi is now live!🎈 Zansi is a Pimp My Book ordering service for university textbooks 📚"
+	},
+	Mutation : {
+		studentDetails: (_, args,context) => data.studentDetails(args, context)
 	},
 };
