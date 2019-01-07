7ba142c156d2b8a96e3cf7edd77ac79388b796b2
amodinho <u14284783@tuks.co.za>
2019-01-07 14:39:28 +0200

    added place order resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -38,12 +38,57 @@ const studentDetails = async (args,context) => {
 		address: args.address};
 }
 
+const placeOrder = async (args, context) => {
+	const params = {
+		TableName: process.env.OrdersDB,
+		Item : {
+			id: uuid.v1(), 
+			userID: args.userID,
+			studentNumber: args.studentNumber,
+			name: args.name,
+			email: args.email,
+			univeristy: args.univeristy,
+			degree: args.degree,
+			bursary: args.bursary,
+			cellNumber: args.cellNumber,
+			address: args.address,
+	   
+			ISBN: args.ISBN,
+			title: args.title,
+			edition: args.edition,
+			author:  args.author,
+			dateOrdered: Date.now(),
+			status: args.status
+		}
+	}
+
+	await dynamoDBLib.call("put", params);
+
+	return {
+		id: args.id,userID: args.userID,
+		studentNumber: args.studentNumber,
+		name: args.name,
+		email: args.email,
+		univeristy: args.univeristy,
+		degree: args.degree,
+		bursary: args.bursary,
+		cellNumber: args.cellNumber,
+		address: args.address,
+   
+		ISBN: args.ISBN,
+		title: args.title,
+		edition: args.edition,
+		author:  args.author,
+		dateOrdered: Date.now(),
+		status: args.status}
+}
 
 export const resolvers = {
 	Query: {
 		hello: () => "Zansi is now live!🎈 Zansi is a Pimp My Book ordering service for university textbooks 📚"
 	},
 	Mutation : {
-		studentDetails: (root, args,context) => studentDetails(args,context)
+		studentDetails: (root, args,context) => studentDetails(args,context),
+		placeOrder: (root,args,context) => placeOrder(args,context)
 	},
 };
