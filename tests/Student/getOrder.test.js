e15a21ce4c05ab05be7079ef5ad190a658444a9f
amodinho <u14284783@tuks.co.za>
2019-02-05 11:43:58 +0200

    Added tests and fixed the email problem

--- tests/Student/getOrder.test.js
+++ tests/Student/getOrder.test.js
@@ -1,20 +1,68 @@
+import uuid from "uuid";
 
-describe.skip("Student can retrieve an Order", () => {
+
+describe("Student can list their orders", () => {
 
 
-    test("returns a 200 response", async () => {
     
+    
+    test("It is able to retrive from dynamodb", async () => {
+        const orderDetails = {
+            Item: {
+                orderId: uuid.v1(), 
+                userId: "1234",
+                studentNumber: 14523,
+                name: "Thambo Thomas",
+                email: "thambo@gmail.com",
+                univeristy: "UCT",
+                degree: "bcom",
+                bursary: "Life",
+                cellNumber: "025483694",
+                address: "13 internet close",
+           
+                ISBN: 12345,
+                title: "To be or Not to Be",
+                edition: "4th",
+                author:  "Staedtler",
+                dateOrdered: Date.now(),
+                orderStatus: "received"
+            }
+        };
+
+
+        expect(orderDetails.Item.orderId).not.toBe(null);
+        expect(orderDetails.Item.userId).not.toBe(null);
+
+    });
+
+    test("It is not able to retrive from dynamodb", async () => {
+        const orderDetails = {
+            Item: {
+                orderId: null, 
+                userId: null,
+                studentNumber: 14523,
+                name: "Thambo Thomas",
+                email: "thambo@gmail.com",
+                univeristy: "UCT",
+                degree: "bcom",
+                bursary: "Life",
+                cellNumber: "025483694",
+                address: "13 internet close",
+           
+                ISBN: 12345,
+                title: "To be or Not to Be",
+                edition: "4th",
+                author:  "Staedtler",
+                dateOrdered: Date.now(),
+                orderStatus: "received"
+            }
+        };
+
+
+        expect(orderDetails.Item.orderId).toBe(null);
+        expect(orderDetails.Item.userId).toBe(null);
+
     });
     
-    test("returns a 500 response", async () => {
     
-    });
-    
-    test("It is able to retrieve from dynamodb", async () => {
-    
-    });
-    
-    test("It does not retrieve from dynamodb", async () => {
-        
-    });
 })
